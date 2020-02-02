b3da587ac30f416fb15179a1bcfd4529ca7cba5e
Nick Nance <nance.nick@gmail.com>
2020-02-01 21:33:14 -0800

    eslint and prettier setup and code formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-const App = () => {
+const App = (): React.ReactElement => {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +21,6 @@ const App = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
