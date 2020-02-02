39c9744456c7eba7c71becb79a5c2574b72cbce3
Nick Nance <nance.nick@gmail.com>
2020-02-02 13:40:50 -0800

    block picker and tester

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,61 @@
 import React from "react";
 import "./App.css";
-import NextPiece, { StraightPiece } from "./components/NextPiece";
+import NextPiece, {
+  IBlock,
+  GamePiece,
+  rotateRight,
+  AllBlocks
+} from "./components/NextPiece";
 
-const App = (): React.ReactElement => {
+type NextPieceProps = {
+  piece: GamePiece;
+};
+
+const NextPieceContainer: React.FC<NextPieceProps> = props => {
+  const [state, setState] = React.useState(props.piece);
+  const [blockIdx, setBlockIdx] = React.useState(0);
+
+  const keyHandler = React.useCallback(
+    (ev: KeyboardEvent): void => {
+      if (ev.keyCode === 38) setState(rotateRight(state));
+      if (ev.keyCode === 39) {
+        blockIdx < AllBlocks.length - 1
+          ? setBlockIdx(blockIdx + 1)
+          : setBlockIdx(0);
+      }
+    },
+    [state, blockIdx]
+  );
+
+  React.useEffect(() => {
+    window.addEventListener("keydown", keyHandler);
+    return (): void => {
+      window.removeEventListener("keydown", keyHandler);
+    };
+  });
+
+  React.useEffect(() => setState(AllBlocks[blockIdx]), [blockIdx]);
+
+  return <NextPiece piece={state} />;
+};
+
+const App: React.FC<{}> = () => {
   return (
-    <div id="gameArea" className="row">
-      <div id="controls" className="col-md-4 d-none d-md-block"></div>
-      <div id="gameboard" className="col-md-4 col-8"></div>
-      <div id="nextpiece" className="col-md-4 col-4">
-        <div className="d-none d-md-block">
-          <b>Next Piece</b>
+    <div id="main" className="App">
+      <div id="gameArea" className="row">
+        <div
+          id="controls"
+          className="col-md-4 d-none d-md-block ControlsCol"
+        ></div>
+        <div id="gameboard" className="col-md-4 col-8"></div>
+        <div id="nextpiece" className="col-md-4 col-4">
+          <div className="d-none d-md-block">
+            <b>Next Piece</b>
+            <br />
+          </div>
           <br />
+          <NextPieceContainer piece={IBlock} />
         </div>
-        <br />
-        <NextPiece piece={StraightPiece} />
       </div>
     </div>
   );
--- src/components/NextPiece.tsx
+++ src/components/NextPiece.tsx
@@ -1,24 +1,97 @@
 import React from "react";
 import "./NextPiece.css";
+import { cloneDeep } from "lodash";
 
 type GameRow = [number, number, number, number, number];
-type GamePiece = [GameRow, GameRow, GameRow, GameRow];
+export type GamePiece = [GameRow, GameRow, GameRow, GameRow, GameRow];
 
-export const StraightPiece: GamePiece = [
-  [0, 0, 1, 0, 0],
-  [0, 0, 1, 0, 0],
-  [0, 0, 1, 0, 0],
-  [0, 0, 1, 0, 0]
-];
-
-export const CubePiece: GamePiece = [
+export const Block: GamePiece = [
   [0, 0, 0, 0, 0],
-  [0, 1, 1, 0, 0],
-  [0, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0]
 ];
 
-const NextPiece = (props: { piece: GamePiece }): React.ReactElement => {
+export const IBlock: GamePiece = [
+  [0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0]
+];
+
+export const JBlock: GamePiece = [
+  [0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0],
+  [0, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0]
+];
+
+export const ZBlock: GamePiece = [
+  [0, 0, 0, 0, 0],
+  [0, 1, 1, 0, 0],
+  [0, 0, 1, 1, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0]
+];
+
+export const TBlock: GamePiece = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0]
+];
+
+export const SBlock: GamePiece = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 0],
+  [0, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0]
+];
+
+export const LBlock: GamePiece = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0],
+  [0, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0]
+];
+
+export const OBlock: GamePiece = [
+  [0, 0, 0, 0, 0],
+  [0, 1, 1, 0, 0],
+  [0, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0]
+];
+
+export const AllBlocks = [
+  IBlock,
+  JBlock,
+  ZBlock,
+  TBlock,
+  SBlock,
+  LBlock,
+  OBlock
+];
+
+export const rotateRight = (piece: GamePiece): GamePiece => {
+  const gridSize = piece.length;
+  return piece.reduce((prev, row, rowIdx) => {
+    row.forEach((col, idx) => {
+      if (idx < gridSize) prev[idx][gridSize - 1 - rowIdx] = col;
+    });
+    return prev;
+  }, cloneDeep(Block));
+};
+
+type NextPieceProps = { piece: GamePiece };
+
+const NextPiece: React.FC<NextPieceProps> = props => {
   return (
     <table>
       <tbody>
