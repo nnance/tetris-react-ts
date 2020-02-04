d1f2a048666dff234d2315699b3363c9d6bd95c3
Nick Nance <nance.nick@gmail.com>
2020-02-04 08:21:26 -0800

    fixed react hook dependency warning

--- src/hooks/useKeyPress.tsx
+++ src/hooks/useKeyPress.tsx
@@ -16,26 +16,26 @@ export default function useKeyPress(targetKey: KeyPressProps): boolean {
   // State for keeping track of whether key is pressed
   const [keyPressed, setKeyPressed] = React.useState(false);
 
-  // If pressed key is our target key then set to true
-  function downHandler({ key, keyCode }: KeyboardEvent): void {
-    if (targetKey.key && key === targetKey.key) {
-      setKeyPressed(true);
-    } else if (targetKey.keyCode && keyCode === targetKey.keyCode) {
-      setKeyPressed(true);
-    }
-  }
-
-  // If released key is our target key then set to false
-  const upHandler = ({ key, keyCode }: KeyboardEvent): void => {
-    if (targetKey.key && key === targetKey.key) {
-      setKeyPressed(false);
-    } else if (targetKey.keyCode && keyCode === targetKey.keyCode) {
-      setKeyPressed(false);
-    }
-  };
-
   // Add event listeners
   React.useEffect(() => {
+    // If pressed key is our target key then set to true
+    function downHandler({ key, keyCode }: KeyboardEvent): void {
+      if (targetKey.key && key === targetKey.key) {
+        setKeyPressed(true);
+      } else if (targetKey.keyCode && keyCode === targetKey.keyCode) {
+        setKeyPressed(true);
+      }
+    }
+
+    // If released key is our target key then set to false
+    const upHandler = ({ key, keyCode }: KeyboardEvent): void => {
+      if (targetKey.key && key === targetKey.key) {
+        setKeyPressed(false);
+      } else if (targetKey.keyCode && keyCode === targetKey.keyCode) {
+        setKeyPressed(false);
+      }
+    };
+
     window.addEventListener("keydown", downHandler);
     window.addEventListener("keyup", upHandler);
     // Remove event listeners on cleanup
@@ -43,7 +43,7 @@ export default function useKeyPress(targetKey: KeyPressProps): boolean {
       window.removeEventListener("keydown", downHandler);
       window.removeEventListener("keyup", upHandler);
     };
-  }, []); // Empty array ensures that effect is only run on mount and unmount
+  }, [targetKey]); // Empty array ensures that effect is only run on mount and unmount
 
   return keyPressed;
 }
