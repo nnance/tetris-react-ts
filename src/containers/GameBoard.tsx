07f1c0e5112538627c97467a0163b969f6ff0257
Nick Nance <nance.nick@gmail.com>
2020-02-05 07:21:15 -0800

    add some todos

--- src/containers/GameBoard.tsx
+++ src/containers/GameBoard.tsx
@@ -4,6 +4,12 @@ import GameBoard from "../components/GameBoard";
 import useKeyPress, { KeyCode } from "../hooks/useKeyPress";
 import { drawIBlock, moveIBlock, rotateIBlock } from "../state/IBlock";
 
+// TODO: maintain rotation state so it draws the correct orientation 
+// TODO: don't draw piece until space bar is pressed
+// TODO: implement edge detection for right and left movement
+// TODO: detect the bottom of the board and stop movement
+// TODO: impelement the current piece so that the board and the next piece is correct
+
 const board: DrawableGrid = Array(20)
   .fill(0)
   .map(x => Array(10).fill(0));
