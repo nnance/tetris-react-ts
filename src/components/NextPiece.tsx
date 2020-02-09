0dba8725b98b1e2a977d2f2fb12479677496928f
Nick Nance <nance.nick@gmail.com>
2020-02-09 07:43:08 -0800

    fix next piece text alignment

--- src/components/NextPiece.tsx
+++ src/components/NextPiece.tsx
@@ -6,22 +6,29 @@ type NextPieceProps = { grid: DrawableGrid };
 
 const NextPiece: React.FC<NextPieceProps> = props => {
   return (
-    <table>
-      <tbody>
-        {props.grid.map((row, rowIdx) => (
-          <tr key={rowIdx}>
-            {row.map((col, idx) => (
-              <td
-                key={`${rowIdx}, ${idx}`}
-                className={col ? "PieceBlock" : "EmptyBlock"}
-              >
-                {" "}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="col-md-4 col-4 nextpiece">
+      <div className="d-none d-md-block">
+        <b>Next Piece</b>
+        <br />
+      </div>
+      <br />
+      <table>
+        <tbody>
+          {props.grid.map((row, rowIdx) => (
+            <tr key={rowIdx}>
+              {row.map((col, idx) => (
+                <td
+                  key={`${rowIdx}, ${idx}`}
+                  className={col ? "PieceBlock" : "EmptyBlock"}
+                >
+                  {" "}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
